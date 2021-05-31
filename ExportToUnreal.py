dc1b5e12e9f553dc43aa738b7f96814ea86ad34c
Uday.Kiran <uday.kiran@avrioc.com>
2021-05-31 18:09:32 +0400

    skinnedMesh updates

--- ExportToUnreal.py
+++ ExportToUnreal.py
@@ -1,6 +1,6 @@
 import maya.cmds as cmds
 import maya.mel as mel
-import os,sys
+import os
 
 class TestUI(object):
     
@@ -25,7 +25,7 @@ class TestUI(object):
         export_fbx_layour = cmds.frameLayout( label=' FBX Mesh Export',collapsable=True,parent=main_layout)
         cmds.button( label='Export Selected Static Mesh',w=220,command='')
         cmds.button( label='Export Selected Skinned Mesh',w=220,command='')
-        cmds.button( label='Export  Character Mesh',w=220,command='')
+        cmds.button( label='Export  Character Mesh',w=220,command=lambda *_:ExportCharacterBaseMesh())
         cmds.button( label='Export  Bicycle Mesh',w=220,command='')
     
         cmds.showWindow(main_window)
@@ -35,7 +35,6 @@ class TestUI(object):
     def delete_window(cls):
         if cmds.window(cls.WINDOW_NAME, exists=True):
             cmds.deleteUI(cls.WINDOW_NAME, window=True)
-
 
 def ExportStaticMeshes():
     pass
@@ -50,8 +49,8 @@ def CreateExportSets():
     pass
 
 def BakeHumanIK():
-    char = getCurrentCharacter()
-    bakeAnim(char)
+    setCurrentCharacter('Character1')
+    mel.eval('hikBakeCharacter 0;')
    
     
 def getCurrentCharacter():
@@ -80,55 +79,40 @@ def getCharacterNodes(char):
 		raise Exception('Invalid character definition node! Object "'+char+'" does not exist or is not a valid HIKCharacterNode!')
 	
 	# Get Character Nodes
-	charNodes = mel.eval('hikGetSkeletonNodes "'+char+'"')
+	charNodes = mel.eval('hikGetSkeletonNodes "'+ char +'"')
 	
 	# Return Result
 	return charNodes
 
-def bakeAnim(char):
-
-	# Get Character Bones
-	bones = getCharacterNodes(char)
-	
-	# Bake Animation
-	cmds.bakeResults(bones,
-					simulation=True,
-					t=(0,1),
-					sampleBy=1,
-					disableImplicitControl=True,
-					preserveOutsideKeys=False,
-					sparseAnimCurveBake=False,
-					removeBakedAttributeFromLayer=False,
-					bakeOnOverrideLayer=False,
-					minimizeRotation=False,
-					at=['tx','ty','tz','rx','ry','rz'] )
-	
-	# Return Result
-	return bones
 
 
 def PrepareFile():
-    filename = saveTempFile()
-    openFile(filename)
+    saveTempFile()
     BakeHumanIK()
     removeSelectedObject()
+    cmds.file(save=True, type='mayaAscii' )
+    
 
 def getFilename():
+    path = {}
     currentFilename = cmds.file( query=True,sceneName=True )
+    currentFilepath = os.path.dirname(currentFilename)
     baseName = os.path.basename(currentFilename)
-    return baseName
+    path['filepath'] = currentFilepath
+    path['filename'] = baseName
+    return path
     
 def openFile(filename):
     cmds.file(newFile=True)
-    cmds.file(filename, open=True)
+    cmds.file(filename, i=True)
 
 def saveTempFile():
     savePath= "C:/Users/BOXX/Desktop/EXPORT_TEST"
-    saveName = getFilename()
+    gFile = getFilename()
+    saveName = gFile['filename'].split(".")[0]
     saveFile = os.path.join(savePath,saveName)
     cmds.file(rename=saveFile)
     cmds.file(save=True,type='mayaAscii')
-    return saveFile
 
 def removeJoints():
     endJointsList = ['HeadTop_End','LeftHandThumb4','LeftHandIndex4','LeftHandRing4','RightHandThumb4','RightHandIndex4','RightHandRing4','LeftToe_End','RightToe_End']
@@ -142,7 +126,6 @@ def removeSelectedObject():
     controls = cmds.ls('Ctrls')
     cmds.delete(controls)
     removeJoints()
-    
 
 def ExportFBXSkinned(filename):
     mel.eval('FBXExportAnimationOnly -v true;')
@@ -153,18 +136,53 @@ def ExportFBXSkinned(filename):
     mel.eval('FBXExport -f' + filename + '-s;')
 
 def ExportCharacterBaseMesh():
-    models = ['sk_male_jersey_G_AvShape','sk_male_body_G_AvShape']
-    for mesh in models:
-        cmds.select(mesh,add=True)
-    rootJoint = cmds.ls('Joints')
-    sel = cmds.select(rootJoint,hierarchy=True)
-    bicycle = cmds.ls('Bicycle*',type='mesh') 
-    cmds.select(bicycle,deselect=True)
+    cmds.select(cl=True)
+    sel = cmds.ls(sl=True)
+    cmds.file(save=True,type='mayaAscii')
+    if len(sel) == 0:
+       cmds.confirmDialog( title='Select Skinnned Meshes', message='Please Select Meshes to be Exported', button=['OK'], defaultButton='OK')
+    else:    
+        currentFilename = cmds.file( query=True,sceneName=True )
+        openFile(currentFilename)
+       
+        # Remove Bicycle Meshes
+        bi = cmds.ls('Bicycle*',type='mesh')
+        cmds.delete(bi)
+        cmds.select(cl=True)
+        
+        # Remove unwanted Meshes
+        cmds.select('All_Geometry')
+        all_Meshes = cmds.ls(sl=True)
+        for x in all_Meshes:
+            if not x in sel:
+                cmds.delete(x)
+                
+        # Remove Display Layers
+        layers = cmds.ls(type='displayLayer')
+        cmds.delete(layers)
+            
+        # Unparent Joints and Meshes
+           
+          
+          
+        # Bake Non Deformer History
+        for node in sel:
+            cmds.bakePartialHistory(node,prePostDeformers=True )
+            cmds.parent(node,w=True)
+        cmds.parent('Joints',w=True)
+        
+        cmds.delete('Assets')
+        cmds.delete('Model')
+        # Remove Unused Nodes
+        mel.eval('hyperShadePanelMenuCommand("hyperShadePanel1", "deleteUnusedNodes");')  
+        
+        multipleFilters = "FBX Files (*.fbx *.FBX)"
+        saveFile = cmds.fileDialog2(fileFilter=multipleFilters, dialogStyle=2)
+        if not saveFile:return
+        ExportFBXSkinned(saveFile)
+        
 
         
-    
-
-
 if __name__ == "__main__":
     TestUI.display()
 
